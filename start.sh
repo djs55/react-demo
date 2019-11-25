1bc6817f6435e8109ba7deb6e617b4bc170dd8a7
David Scott <dave@recoil.org>
2019-11-25 20:46:28 +0000

    chmod +x bin/* (for all bin/)
    Signed-off-by: David Scott <dave@recoil.org>

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 # Restore the execute permission if it has been lost over a file sync from Windows.
 chmod +x node_modules/.bin/*
+find -type d -name bin -exec sh -c "chmod +x {}/*" \;
 
 exec npm start
